0a150dcef43d3f37d616215244e85c496907fb7b
Paweł Strzałkowski <pawel@praesens.co>
2018-03-20 22:45:32 +0100

    Undo change

--- app/model/spree/springboard_resource.rb
+++ app/model/spree/springboard_resource.rb
@@ -1,7 +1,5 @@
 module Spree
   class SpringboardResource < ActiveRecord::Base
-    include Spree::RansackableAttributes
-
     belongs_to :resource, polymorphic: true
     belongs_to :parent, polymorphic: true
   end
