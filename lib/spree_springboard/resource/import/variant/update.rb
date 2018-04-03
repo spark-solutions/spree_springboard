0523772bb8bd0c0edd016d2f325f275919ddc41e
Wojtek <swierczynskiwojtek@gmail.com>
2018-04-03 18:02:40 +0200

    update tax category only if tax category present

--- lib/spree_springboard/resource/import/variant/update.rb
+++ lib/spree_springboard/resource/import/variant/update.rb
@@ -45,12 +45,12 @@ module SpreeSpringboard
               cost_price: (springboard_item.cost if springboard_item.cost != variant.cost_price),
               original_price: (springboard_item.original_price if springboard_item.original_price != variant.original_price),
               sale_price: (springboard_item.price if springboard_item.price != variant.sale_price),
-              tax_category: (tax_category if tax_category != variant.tax_category),
+              tax_category: (tax_category if tax_category.present? && tax_category != variant.tax_category),
               upc: (springboard_item.custom.upc if springboard_item.custom.upc != variant.upc),
               weight: (springboard_item.weight if springboard_item.weight != variant.weight)
             }.compact
 
-            variant.product.update(tax_category: tax_category) if tax_category != variant.product.tax_category
+            variant.product.update(tax_category: tax_category) if tax_category.present? && tax_category != variant.product.tax_category
 
             if values.present?
               variant.update values
